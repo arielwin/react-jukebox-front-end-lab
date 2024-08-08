8d0a95ebe1d3058faacf18f20aadd12d9d4f97b6
arielwin <subiboy7@gmail.com>
2024-08-08 07:21:00 -0600

    added track list and a few other thingies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 // src/App.jsx
 import React from 'react'
 import Home from './components/Home/Home'
-
+import NavBar from './components/NavBar/NavBar';
+import TrackList from './components/TrackList/TrackList';
+import { useState } from 'react'
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const[tracks, setTracks] = useState([])
+
+  //const handlePlayTrack
+  //const handleEdittrack
+  //const handleDeleteTrack
+  //const handleSaveTrack
+
+  return (
+    <>
+      <NavBar />
+      <TrackList tracks={tracks}/>
+      
+    </>
+  )
 };
 
 export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,38 +1,17 @@
 import { Link } from 'react-router-dom';
-import { AuthedUserContext } from '../../App';
+//import { AuthedUserContext } from '../../App';
 import { useContext } from 'react';
 
 const NavBar = ({ handleSignout }) => {
-  const user = useContext(AuthedUserContext);
   return (
     <>
-      {user ? (
-        <nav>
-          <ul>
-            <li>Welcome, {user.username}</li>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="" onClick={handleSignout}>
-                Sign Out
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      ) : (
-        <nav>
-          <ul>
-            <li>
-              <Link to="/signin">Sign In</Link>
-            </li>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-          </ul>
-        </nav>
-      )}
+      <ul>
+        <li><Link to='/'>HOME</Link></li>
+        <li><Link to='/tracks'>SONGS</Link></li>
+
+      </ul>
     </>
   );
 };
+
 export default NavBar;
